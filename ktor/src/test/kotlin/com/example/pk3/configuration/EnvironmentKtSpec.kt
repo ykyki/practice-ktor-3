bb8e62f73de69a9e1dacb2babdffd033ed94eba4
ykyki <yukimath2@gmail.com>
2022-01-16 02:01:40 +0900

    refactor(ktor3): use shouldBeTrue

--- ktor/src/test/kotlin/com/example/pk3/configuration/EnvironmentKtSpec.kt
+++ ktor/src/test/kotlin/com/example/pk3/configuration/EnvironmentKtSpec.kt
@@ -2,7 +2,7 @@ package com.example.pk3.configuration
 
 import io.kotest.assertions.withClue
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.shouldBe
+import io.kotest.matchers.booleans.shouldBeTrue
 import withTestRootModule
 import java.nio.file.Path
 
@@ -24,11 +24,11 @@ class EnvironmentKtSpec : StringSpec({
 })
 
 private fun Path.shouldBePlayable() {
-    withClue("Path should be absolute") { this.isAbsolute shouldBe true }
+    withClue("Path should be absolute") { this.isAbsolute.shouldBeTrue() }
     this.toFile().let { file ->
-        withClue("Path should exist") { file.exists() shouldBe true }
-        withClue("Path should be readable") { file.canRead() shouldBe true }
-        withClue("Path should be writable") { file.canWrite() shouldBe true }
-        withClue("Path should be executable") { file.canExecute() shouldBe true }
+        withClue("Path should exist") { file.exists().shouldBeTrue() }
+        withClue("Path should be readable") { file.canRead().shouldBeTrue() }
+        withClue("Path should be writable") { file.canWrite().shouldBeTrue() }
+        withClue("Path should be executable") { file.canExecute().shouldBeTrue() }
     }
 }
