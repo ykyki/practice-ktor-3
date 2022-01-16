cdce902af20c03af28c0aed716caad5fa71fb544
ykyki <yukimath2@gmail.com>
2022-01-16 16:39:49 +0900

    feat(sub-sample): add notnull annotation

--- sub-sample/src/main/java/com/example/subsample/Sample.java
+++ sub-sample/src/main/java/com/example/subsample/Sample.java
@@ -1,8 +1,11 @@
 package com.example.subsample;
 
+import org.jetbrains.annotations.NotNull;
+
 public final class Sample {
     private final static String PREFIX = "sub-sample-012345:";
 
+    @NotNull
     public String get(String input) {
         return PREFIX + input;
     }
