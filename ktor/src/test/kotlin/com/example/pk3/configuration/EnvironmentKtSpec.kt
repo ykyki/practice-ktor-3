c1d0344f7958d8a565413726b9a75c7aab19880e
ykyki <yukimath2@gmail.com>
2022-02-04 23:39:15 +0900

    fix(ktor3): refactor by kotest assertions

--- ktor/src/test/kotlin/com/example/pk3/configuration/EnvironmentKtSpec.kt
+++ ktor/src/test/kotlin/com/example/pk3/configuration/EnvironmentKtSpec.kt
@@ -1,8 +1,8 @@
 package com.example.pk3.configuration
 
-import io.kotest.assertions.withClue
+import io.kotest.assertions.assertSoftly
 import io.kotest.core.spec.style.StringSpec
-import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.paths.*
 import withTestRootModule
 import java.nio.file.Path
 
@@ -24,11 +24,18 @@ class EnvironmentKtSpec : StringSpec({
 })
 
 private fun Path.shouldBePlayable() {
-    withClue("Path should be absolute") { this.isAbsolute.shouldBeTrue() }
-    this.toFile().let { file ->
-        withClue("Path should exist") { file.exists().shouldBeTrue() }
-        withClue("Path should be readable") { file.canRead().shouldBeTrue() }
-        withClue("Path should be writable") { file.canWrite().shouldBeTrue() }
-        withClue("Path should be executable") { file.canExecute().shouldBeTrue() }
+    assertSoftly(this) {
+        shouldBeAbsolute()
+        shouldExist()
+        shouldBeReadable()
+        shouldBeWriteable()
+        shouldBeExecutable()
     }
+    // withClue("Path should be absolute") { this.isAbsolute.shouldBeTrue() }
+    // this.toFile().let { file ->
+    //     withClue("Path should exist") { file.exists().shouldBeTrue() }
+    //     withClue("Path should be readable") { file.canRead().shouldBeTrue() }
+    //     withClue("Path should be writable") { file.canWrite().shouldBeTrue() }
+    //     withClue("Path should be executable") { file.canExecute().shouldBeTrue() }
+    // }
 }
