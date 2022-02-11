7028adf06417a41afcd1ba4bea0f4ff8c89e7b4e
ykyki <yukimath2@gmail.com>
2022-02-11 22:32:41 +0900

    feat(ktor3): update CustomerRouterKtSpec

--- ktor/src/test/kotlin/com/example/pk3/router/CustomerRouterKtSpec.kt
+++ ktor/src/test/kotlin/com/example/pk3/router/CustomerRouterKtSpec.kt
@@ -3,7 +3,7 @@ package com.example.pk3.router
 import com.example.pk3.domain.tutorial.customer.Customer
 import io.kotest.assertions.ktor.shouldHaveContent
 import io.kotest.assertions.ktor.shouldHaveStatus
-import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.string.shouldStartWith
 import io.ktor.http.*
 import io.ktor.server.testing.*
@@ -12,12 +12,13 @@ import kotlinx.serialization.json.Json
 import shouldEqualJsonStrictly
 import withTestRootModule
 
-class CustomerRouterKtSpec : StringSpec({
-    "GET: /customer" {
-        withTestRootModule {
-            handleRequest(method = HttpMethod.Get, uri = "/customer").apply {
-                response shouldHaveStatus HttpStatusCode.OK
-                response.content!! shouldEqualJsonStrictly """
+class CustomerRouterKtSpec : FunSpec({
+    context("/customer") {
+        test("GET") {
+            withTestRootModule {
+                handleRequest(method = HttpMethod.Get, uri = "/customer").apply {
+                    response shouldHaveStatus HttpStatusCode.OK
+                    response.content!! shouldEqualJsonStrictly """
                     [
                         {
                             "id": 0,
@@ -27,31 +28,15 @@ class CustomerRouterKtSpec : StringSpec({
                         }
                     ]
                 """
+                }
             }
         }
-    }
-    "GET: /customer/abc000 should return BadRequest" {
-        withTestRootModule {
-            handleRequest(method = HttpMethod.Get, uri = "/customer/abc000").apply {
-                response shouldHaveStatus HttpStatusCode.BadRequest
-                response shouldHaveContent "Request parameter id couldn't be parsed/converted to Long"
-            }
-        }
-    }
-    "GET: /customer/9999 should return NotFound" {
-        withTestRootModule {
-            handleRequest(method = HttpMethod.Get, uri = "/customer/9999").apply {
-                response shouldHaveStatus HttpStatusCode.NotFound
-                response shouldHaveContent "No customer found (id = 9999)"
-            }
-        }
-    }
-    "POST(correct customer): /customer" {
-        withTestRootModule {
-            handleRequest(method = HttpMethod.Post, uri = "/customer") {
-                addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
-                setBody(
-                    """
+        test("POST:正当な値") {
+            withTestRootModule {
+                handleRequest(method = HttpMethod.Post, uri = "/customer") {
+                    addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                    setBody(
+                        """
                           {
                             "id": 1234567890,
                             "firstName": "太郎",
@@ -59,21 +44,40 @@ class CustomerRouterKtSpec : StringSpec({
                             "email": "test@example.com"
                           }
                 """
-                )
-            }.apply {
-                response shouldHaveStatus HttpStatusCode.Created
-                response.content shouldStartWith "Customer stored correctly!:"
+                    )
+                }.apply {
+                    response shouldHaveStatus HttpStatusCode.Created
+                    response.content shouldStartWith "Customer stored correctly!:"
+                }
+            }
+        }
+        test("POST:正当な値2") {
+            withTestRootModule {
+                handleRequest(method = HttpMethod.Post, uri = "/customer") {
+                    addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                    setBody(Json.encodeToString(Customer(546, "花子", "佐藤", "test@example.com")))
+                }.apply {
+                    response shouldHaveStatus HttpStatusCode.Created
+                    response.content shouldStartWith "Customer stored correctly!:"
+                }
             }
         }
     }
-    "POST(correct customer 2): /customer" {
-        withTestRootModule {
-            handleRequest(method = HttpMethod.Post, uri = "/customer") {
-                addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
-                setBody(Json.encodeToString(Customer(546, "花子", "佐藤", "test@example.com")))
-            }.apply {
-                response shouldHaveStatus HttpStatusCode.Created
-                response.content shouldStartWith "Customer stored correctly!:"
+    context("/customer/*") {
+        test("GET:数値ではないときにはBadRequest") {
+            withTestRootModule {
+                handleRequest(method = HttpMethod.Get, uri = "/customer/abc000").apply {
+                    response shouldHaveStatus HttpStatusCode.BadRequest
+                    response shouldHaveContent "Request parameter id couldn't be parsed/converted to Long"
+                }
+            }
+        }
+        test("GET:存在しないときにはNotFound") {
+            withTestRootModule {
+                handleRequest(method = HttpMethod.Get, uri = "/customer/9999").apply {
+                    response shouldHaveStatus HttpStatusCode.NotFound
+                    response shouldHaveContent "No customer found (id = 9999)"
+                }
             }
         }
     }
